3f5fc7e3e0b35ff6db685410b9451b735aa43c2d
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-01-19 17:51:07 -0300

    try catch

--- async_await/async_await.ts
+++ async_await/async_await.ts
@@ -1,4 +1,5 @@
-// async function indicates that there will be a value to be expected from a promise through await
+// The word “async” before a function means one simple thing: a function always returns a promise.
+// Other values are wrapped in a resolved promise automatically.
 
 function firstFunction() {
     return new Promise((resolve: Function) => {
@@ -11,9 +12,16 @@ function firstFunction() {
 
 async function secondFunction() {
     console.log("Started")
-    return await firstFunction() // pause execution and returns only after receiving the value from the promise
+
+    try { // await should always be enclosed by try-catch block
+        return await firstFunction() // pause execution and returns only after receiving the value from the promise
+    } catch (error) {
+        alert(error)
+    }
 }
 
 secondFunction().then((value) => {
     console.log("Finished - value: " + value);
+}).catch(err => {
+    alert(err)
 })
